ffa7c836a24c83321ce83a9599e4483ff3f0479d
[-=KILL MAN=-] <mctesting1@yandex.ru>
2025-10-30 23:54:06 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,9 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_MODULE    := NCOO
-LOCAL_SRC_FILES := main.cpp mod/logger.cpp
-LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17 -mfpu=neon
-LOCAL_C_INCLUDES += ./include
-LOCAL_LDLIBS += -llog
+ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+	LOCAL_MODULE   := NCOO
+else
+	LOCAL_MODULE   := NCOO64
+endif
+LOCAL_SRC_FILES  := main.cpp mod/logger.cpp
+LOCAL_CFLAGS     += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17 -mfpu=neon
+LOCAL_LDLIBS     += -llog
 include $(BUILD_SHARED_LIBRARY)
